cb160d115dbd96ed57f7a01a7395da705ebe3d71
Samuel Javal <samueljaval@gmail.com>
2020-05-04 19:34:26 +0200

    overtime_plots

---
+++ overtime_test.py
@@ -0,0 +1,50 @@
+from single_test import get_speeds
+import time
+import os
+import matplotlib.pyplot as plt
+
+def get_input():
+    return 1, 1
+
+def overtime_test(download, upload):
+    overall, interval = get_input()
+    start = time.time()
+    get_time = time.time()
+    datetimes = []
+    downloads_lst = []
+    uploads_lst = []
+    while get_time - start < overall*60:
+        before = time.time()
+        # chosen timestamp for a single test is right before the test rather than right after
+        datetimes.append((time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(before)))[5:])
+        print(datetimes[-1])
+        st = get_speeds(download,upload)
+        if download == 1:
+            downloads_lst.append(float(st[0]))
+        if upload == 1:
+            uploads_lst.append(float(st[1]))
+        after = time.time()
+        time.sleep(interval*60 - (after - before))
+        get_time = time.time()
+    return downloads_lst, uploads_lst, datetimes
+
+def get_plot(download, upload):
+    d, u, t = overtime_test(1,1)
+    if download == 1 :
+        plt.figure()
+        plt.plot(t,d)
+        plt.title("Results of Download speed analysis (in Mbit/s)",loc='center')
+        plt.xticks(rotation='vertical')
+        plt.ylabel('download speed in Mbit/s')
+        plt.tight_layout()
+        plt.savefig('download_speeds.png')
+    if upload == 1:
+        plt.figure()
+        plt.plot(t,u)
+        plt.title("Results of Upload speed analysis (in Mbit/s)",loc='center')
+        plt.xticks(rotation='vertical')
+        plt.ylabel('upload speed in Mbit/s')
+        plt.tight_layout()
+        plt.savefig('upload_speeds.png')
+
+get_plot(1,1)
